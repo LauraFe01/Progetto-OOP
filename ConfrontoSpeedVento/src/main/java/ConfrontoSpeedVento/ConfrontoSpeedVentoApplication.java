b93a7e1e1f9b32bb2ecf3b341a1566569259ba4e
Laura Ferretti <s1098036@studenti.univpm.it>
2021-12-06 21:14:10 +0100

    Aggiunta file SpringBoot

---
+++ ConfrontoSpeedVento/src/main/java/ConfrontoSpeedVento/ConfrontoSpeedVentoApplication.java
@@ -0,0 +1,13 @@
+package ConfrontoSpeedVento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfrontoSpeedVentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfrontoSpeedVentoApplication.class, args);
+	}
+
+}
---
+++ ConfrontoSpeedVento/src/test/java/ConfrontoSpeedVento/ConfrontoSpeedVentoApplicationTests.java
@@ -0,0 +1,13 @@
+package ConfrontoSpeedVento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfrontoSpeedVentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
