cc774278453e10d717b2756bbd19cf4ff3be703b
Edoardo Cecchini <S1101827@studenti.univpm.it>
2021-12-07 12:41:44 +0100

    Prima versione della classe città

--- ConfrontoSpeedVento/src/main/java/ConfrontoSpeedVento/model/Citta.java
+++ ConfrontoSpeedVento/src/main/java/ConfrontoSpeedVento/model/Citta.java
@@ -1,5 +1,42 @@
 package ConfrontoSpeedVento.model;
 
-public class Citta {
+import java.util.*;
 
+public class Citta {
+	private String nome;
+	private long idOpenW;
+	private vector<datoVento> datiVento;
+	
+	public Citta(String nome,long idOpenW)
+	{
+		this.nome=nome;
+		this.idOpenW=idOpenW;
+		this.datiVento = new Vector<datoVento>();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getIdOpenW() {
+		return idOpenW;
+	}
+
+	public void setIdOpenW(long idOpenW) {
+		this.idOpenW = idOpenW;
+	}
+	
+	public Vector<datoVento> getDatiVento()
+	{
+		return this.datiVento;
+	}
+	
+	public void setDatiVento(Vector<datoVento> datiVento) {
+		this.datiVento = datiVento;
+	}
+	
 }
