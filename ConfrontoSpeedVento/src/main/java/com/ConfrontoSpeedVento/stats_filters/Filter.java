dfb316f410f4869ec16e00a6778bf8172f635a83
Laura Ferretti <s1098036@studenti.univpm.it>
2021-12-15 10:54:44 +0100

    Creazione package stats_filters

--- ConfrontoSpeedVento/src/main/java/com/ConfrontoSpeedVento/stats_filters/Filter.java
+++ ConfrontoSpeedVento/src/main/java/com/ConfrontoSpeedVento/stats_filters/Filter.java
@@ -12,56 +12,56 @@ public class Filter {
 	private String nomeCitta;
 	private String dataInizio;
 	private String dataFine;
-	
+
 	public Filter(String nomeCitta, String dataInizio, String dataFine)
 	{
 		this.nomeCitta= nomeCitta;
 		this.dataInizio = dataInizio;
 		this.dataFine = dataFine;
 	}
-	
+
 	public Vector<Double> FilterCustomRange()
 	{
 		String route = System.getProperty("user.dir") + "/src/main/resources/" + nomeCitta + "SalvataggioOrario.json";
-		
+
 		Vector<Double> datiSpeed=new Vector<Double>();
-	
+
 		boolean flag=false;
 		String line = null;
 
-	    try {
-	        FileReader fileReader = new FileReader(route);
+		try {
+			FileReader fileReader = new FileReader(route);
 
-	        BufferedReader bufferedReader = new BufferedReader(fileReader);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-	        while((line = bufferedReader.readLine()) != null) {
+			while((line = bufferedReader.readLine()) != null) {
 
-	            JSONTokener tokener = new JSONTokener(line);
-	            JSONObject object = new JSONObject(tokener);
-	            
-	            if(dataInizio.equals((String)object.get("data")))
-	              {  
-	            	flag=true;
-	              }
-	            
-	            if(flag==true)
-	            {
-	            	datiSpeed.add((double)object.get("speed"));
-	            }
-	            
-	            if(dataFine.equals((String) object.get("data")))
-	              {
-	            	flag=false;
-	              }
-		        
-	        }
-	        bufferedReader.close(); 
-	       
-	        return datiSpeed;
-			
+				JSONTokener tokener = new JSONTokener(line);
+				JSONObject object = new JSONObject(tokener);
+
+				if(dataInizio.equals((String)object.get("data")))
+				{  
+					flag=true;
+				}
+
+				if(flag==true)
+				{
+					datiSpeed.add((double)object.get("speed"));
+				}
+
+				if(dataFine.equals((String) object.get("data")))
+				{
+					flag=false;
+				}
+
+			}
+			bufferedReader.close(); 
+
+			return datiSpeed;
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	    return datiSpeed;
+		return datiSpeed;
 	}
 }
